c84e25c322ec615407ec0fc5ca7d57bc8d7bfd7a
Heng <bunheng.try@student.cadt.edu.kh>
2025-01-30 23:39:05 +0700

    Done

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -3,4 +3,15 @@ public class PaymentSystem {
     private double amount;
     private String status;
     private String payment_Date;
+
+    PaymentSystem(int payment_ID, double amount, String status, String payment_Date){
+        this.payment_ID = payment_ID;
+        this.amount = amount;
+        this.status = status;
+        this.payment_Date = payment_Date;
+    }
+    public String Status(){
+        return "";
+    }
+    
 }
